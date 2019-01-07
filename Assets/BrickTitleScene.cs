a778e46a37da95ba132f733e29652186ee729f2c
Namphibian <Namphibian@users.noreply.github.com>
2019-01-07 15:35:20 +1100

    Added Title Screen
    Added a title screen.

---
+++ Assets/BrickTitleScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BrickTitleScene : MonoBehaviour
+{
+    public Texture backgroundTexture;
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("A key or mouse click has been detected");
+            
+            SceneManager.LoadScene("BrickScene");
+        }
+    }
+}
